878beb8c337bbe2da30f8782cf3c59ce0279d4ff
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-04-21 00:16:37 +0530

    used activatedRoute in home components for search item

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
 import { StarRatingComponent } from 'ng-starrating';
 import { Foods } from '../shared/models/food';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,7 @@ import { Foods } from '../shared/models/food';
 })
 export class HomeComponent implements OnInit {
   foods: Foods[] = [];
-  constructor(private fs: FoodService) {}
+  constructor(private fs: FoodService, private router: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.foods = this.fs.getAll();
