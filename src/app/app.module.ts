b00baedf4eb466a89f0a949a3a7513dcfed742dd
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-04-21 00:25:16 +0530

    created search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { RatingModule } from 'ng-starrating';
+import { SearchComponent } from './search/search.component';
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent],
   imports: [BrowserModule, AppRoutingModule, RatingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,9 +11,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
   foods: Foods[] = [];
-  constructor(private fs: FoodService, private router: ActivatedRoute) {}
+  constructor(private fs: FoodService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.foods = this.fs.getAll();
+    this.route.params.subscribe((params) => {
+      if (params['searchItem'])
+        this.foods = this.fs
+          .getAll()
+          .filter((food) =>
+            food.name.toLowerCase().includes(params['searchItem'].toLowerCase())
+          );
+      else this.foods = this.fs.getAll();
+    });
   }
 }
