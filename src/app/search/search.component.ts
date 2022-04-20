5d3156e5884a9febd7358527bc607990dd31b165
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-04-21 00:29:33 +0530

    added SearchItem property

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  searchItem: string = '';
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.searchItem = params['searchItem'];
+    });
   }
-
 }
